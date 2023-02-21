74449711e2cb0f0fb11931cced7b1953b4116d71
naufalrays <rayhannaufalherlano@gmail.com>
2023-02-21 08:45:21 +0700

    first commit, add feature dialog flow

---
+++ lib/view/home_view.dart
@@ -0,0 +1,89 @@
+import 'package:dialog_flowtter/dialog_flowtter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test_dialogflow/components/messages.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  late DialogFlowtter dialogFlowtter;
+  final TextEditingController _messageController = TextEditingController();
+  List<Map<String, dynamic>> messages = [];
+
+  @override
+  void initState() {
+    DialogFlowtter.fromFile().then((value) => dialogFlowtter = value);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Test dialog flow"),
+        elevation: 2,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: MessagesScreen(messages: messages),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 14,
+                vertical: 8,
+              ),
+              color: Colors.grey.shade800,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _messageController,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      sendMessage(_messageController.text);
+                      _messageController.clear();
+                    },
+                    icon: const Icon(Icons.send),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  sendMessage(String text) async {
+    if (text.isEmpty) {
+      print('Message is Empty');
+    } else {
+      setState(() {
+        addMessage(Message(text: DialogText(text: [text])), true);
+      });
+      DetectIntentResponse response = await dialogFlowtter.detectIntent(
+        queryInput: QueryInput(text: TextInput(text: text)),
+      );
+      if (response.message == null) return;
+      setState(() {
+        addMessage(response.message!);
+      });
+    }
+  }
+
+  addMessage(Message message, [bool isUserMessage = false]) {
+    // print('response data: ${message.payload}');
+    // print(message.payload!["title"][0]);
+    // print(message.payload?["richContent"][0][0]["subtitle"]);
+    messages.add({"message": message, "isUserMessage": isUserMessage});
+  }
+}
